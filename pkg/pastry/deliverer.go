94792216970d68bb0302102638aa033c96067bf0
Thomas <9749173+uhthomas@users.noreply.github.com>
2019-05-13 17:43:15 +0100

    Add docs

--- pkg/pastry/deliverer.go
+++ pkg/pastry/deliverer.go
@@ -1,9 +1,12 @@
 package pastry
 
+// Deliverer will be called when the node is the closest the message can be routed to.
 type Deliverer interface {
 	Deliver(key, b []byte)
 }
 
+// DelivererFunc is an adapter to allow the use of ordinary functions as Deliverers.
 type DelivererFunc func(key, b []byte)
 
+// Deliver calls f(key, b)
 func (f DelivererFunc) Deliver(key, b []byte) { f(key, b) }
--- pkg/pastry/forwarder.go
+++ pkg/pastry/forwarder.go
@@ -1,9 +1,12 @@
 package pastry
 
+// Forwarder can modify the contents of redirect the message elsewhere or set next to nil to deliver to itself.
 type Forwarder interface {
 	Forward(key, b, next []byte)
 }
 
+// ForwarderFunc is an adapter to allow the use of ordinary functions as Forwarders.
 type ForwarderFunc func(key, b, next []byte)
 
+// Forward calls f(key, b, next)
 func (f ForwarderFunc) Forward(key, b, next []byte) { f(key, b, next) }
